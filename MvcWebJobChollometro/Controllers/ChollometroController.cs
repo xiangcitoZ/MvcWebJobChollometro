a54bb8d1ece396cdd1e6955beecb661bbadffdc3
xiangcitoZ <pzxiang1@gmail.com>
2023-04-12 11:13:07 +0200

    Add project files.

---
+++ MvcWebJobChollometro/Controllers/ChollometroController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcWebJobChollometro.Models;
+using MvcWebJobChollometro.Repositories;
+
+namespace MvcWebJobChollometro.Controllers
+{
+    public class ChollometroController : Controller
+    {
+        private RepositoryChollos repo;
+
+        public ChollometroController(RepositoryChollos repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Chollo> chollos = await this.repo.GetChollosAsync();
+            return View(chollos);
+        }
+    }
+}
---
+++ MvcWebJobChollometro/Repositories/RepositoryChollos.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcWebJobChollometro.Models;
+using MvcWebJobChollometro.Data;
+
+
+namespace MvcWebJobChollometro.Repositories
+{
+    public class RepositoryChollos
+    {
+        private ChollometroContext context;
+
+        public RepositoryChollos(ChollometroContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Chollo>> GetChollosAsync()
+        {
+            var consulta = from datos in this.context.Chollos
+                           orderby datos.Fecha descending
+                           select datos;
+            return await consulta.ToListAsync();
+        }
+
+    }
+}
